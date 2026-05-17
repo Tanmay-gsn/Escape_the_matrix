745afed5dfc0f1c7c134d8d7c6d30466b91a2f76
ranjith sira <ranjithsira8@gmail.com>
2026-05-17 13:47:43 +0530

    Implement movePredictor function for monster movement

---
+++ client/src/game/monsters/predictor.js
@@ -0,0 +1,20 @@
+import { astar } from "../pathfinding/astar.js";
+
+export function movePredictor(
+  monster,
+  player,
+  map
+) {
+
+  const path = astar(
+    map,
+    monster,
+    player
+  );
+
+  if (path.length > 0) {
+
+    monster.x = path[0].x;
+    monster.y = path[0].y;
+  }
+}
